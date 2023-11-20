16281822a8683e7bafb8207b06503463578c6679
Koren Ben Ezra <109624775+Koren-Ben-Ezra@users.noreply.github.com>
2023-11-20 16:11:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,19 +9,23 @@ class PT(Enum):
     PLAYER = 1
     BRICK = 2
     BACKGROUND = 3
-    CEMENT = 4
-    ENEMY = 5
+    PIZZA = 4
+    MINI_PIZZA = 5
+    BOX = 6
+    ENEMY = 7
 
 
 BLOCK_SIZE = 20
 BLOCK_MARGIN = 1
 all_parts = {}
-PART_COLOR = {PT.BRICK: "brown", PT.BACKGROUND: pygame.Color(0, 156, 252), PT.PLAYER: "orange",
-              PT.CEMENT: pygame.Color(201, 201, 201)}
+PART_COLOR = {"brick0": pygame.Color(201, 201, 201), "brick1": "brown", "background": pygame.Color(0, 156, 252),
+              "box0": pygame.Color(166, 101, 51), "box1": pygame.Color(207, 135, 81)}
 PLAYER_COLOR = {"head": "orange", "pants": pygame.Color(40, 93, 191), "shoes": "black",
                 "skin": pygame.Color(212, 157, 99), "belt": "black", "shirt": pygame.Color(124, 100, 232)}
 PLAYER_VELOCITY = {"normal": 10, "crouch": 7}
 PLAYER_JUMP_H = {"normal": 4, "crouch": 2}
+BLOCK_PARTS = (PT.BRICK, PT.BOX)
+BREAKABLE_BLOCKS = (PT.BOX,)
 
 WIDTH, HEIGHT = 1280, 720
 W_BLOCKS, H_BLOCKS = WIDTH // BLOCK_SIZE, HEIGHT // BLOCK_SIZE
@@ -36,31 +40,36 @@ class Direction(Enum):
 
 class Part:
 
-    def __init__(self, x: int, y: int, pt: PT):
+    def __init__(self, x: int, y: int, pt: PT, lifetime: int = None, direction: Direction = None):
+        self.x = x
+        self.y = y
         self.pt = pt
-        self.color = PART_COLOR.get(pt)
-        all_parts[(x, y)] = self
+        self.lifetime = lifetime
+        self.direction = direction
+        if not self.pt == PT.PIZZA:
+            all_parts[(x, y)] = self
 
 
 class Game:
 
     def __init__(self, width: int, height: int):
         pygame.init()
+        self.running = True
         self.screen = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Pizza Boi")
-
         self.player_position = [W_BLOCKS // 2, H_BLOCKS - 3]
         self.player_velocity = PLAYER_VELOCITY.get("normal")
         self.player_crouch = False
         self.player_stuck = [False, False, False]  # [from_left, from_right, from_top]
-        self.jump_velocity = 0
         self.player_direction = Direction.FRONT
+        self.player_jump_height = PLAYER_JUMP_H.get("normal")
+        self.player_jump_velocity = 0
+        self.pizza_velocity = 20
+        self.pizza_lifetime = 1000  # milliseconds
+        self.pizza_objects = []
         self.GRAVITY = 25
-        self.jump_height = PLAYER_JUMP_H.get("normal")
-        self.init_floor()
-
-        Maps.map_0()
+        self.reset()
 
     def handle_keys(self, dt):
         self.player_direction = Direction.FRONT
@@ -79,11 +88,18 @@ class Game:
         if keys[pygame.K_s] or keys[pygame.K_DOWN] or (self.player_crouch and self.player_stuck[2]):
             self.player_crouch = True
             self.player_velocity = PLAYER_VELOCITY.get("crouch")
-            self.jump_height = PLAYER_JUMP_H.get("crouch")
+            self.player_jump_height = PLAYER_JUMP_H.get("crouch")
         else:
             self.player_crouch = False
             self.player_velocity = PLAYER_VELOCITY.get("normal")
-            self.jump_height = PLAYER_JUMP_H.get("normal")
+            self.player_jump_height = PLAYER_JUMP_H.get("normal")
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self._throw_pizza()
 
         self.player_position[0] = round(self.player_position[0], 1)
 
@@ -98,21 +114,37 @@ class Game:
 
     def draw_parts(self):
 
-        left_border_x = self.player_position[0] - W_BLOCKS // 2
-        xy_part_map = [(left_border_x + i, y) for i in range(W_BLOCKS + 1) for y in range(0, H_BLOCKS + 1) if
-                       all_parts.get((math.floor(left_border_x) + i, y)) is not None]
+        left_border_x = math.floor(self.player_position[0] - W_BLOCKS // 2)
 
+        xy_part_map = [(left_border_x + i, y) for i in range(W_BLOCKS + 1) for y in range(0, H_BLOCKS + 1) if
+                       all_parts.get((left_border_x + i, y)) is not None]
         for (x, y) in xy_part_map:
             left, top = (x - left_border_x - round(self.player_position[0] % 1, 1)) * BLOCK_SIZE, y * BLOCK_SIZE
             part = all_parts.get((math.floor(x), y))
             if isinstance(part, Part):
                 self.draw_part(part.pt, left, top)
 
+        for pizza_part in self.pizza_objects:
+            left, top = (pizza_part.x - left_border_x - round(self.player_position[0] % 1,
+                                                              1)) * BLOCK_SIZE, pizza_part.y * BLOCK_SIZE
+            self.draw_part(pizza_part.pt, left, top)
+
     def draw_part(self, pt: PT, left, top):
-        w = h = BLOCK_SIZE - 2 * BLOCK_MARGIN
         if pt == PT.BRICK:
-            pygame.draw.rect(self.screen, PART_COLOR.get(PT.CEMENT), (left, top, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(self.screen, PART_COLOR.get(PT.BRICK), (left + BLOCK_MARGIN, top + BLOCK_MARGIN, w, h))
+            w = h = BLOCK_SIZE - 2 * BLOCK_MARGIN
+            pygame.draw.rect(self.screen, PART_COLOR.get("brick0"), (left, top, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.screen, PART_COLOR.get("brick1"), (left + BLOCK_MARGIN, top + BLOCK_MARGIN, w, h))
+        if pt == PT.BOX:
+            w = h = BLOCK_SIZE - 6 * BLOCK_MARGIN
+            pygame.draw.rect(self.screen, PART_COLOR.get("box0"), (left, top, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.screen, PART_COLOR.get("box1"), (left + 3*BLOCK_MARGIN, top + 3*BLOCK_MARGIN, w, h))
+
+        if pt == PT.PIZZA:
+            pygame.draw.rect(self.screen, "red",
+                             (left, top + 10, BLOCK_SIZE, 5))
+
+        if pt == PT.MINI_PIZZA:
+            pygame.draw.rect(self.screen, "red", (left, top + 5, BLOCK_SIZE // 2, 3))
 
     def draw_player(self):
         size = BLOCK_SIZE
@@ -158,7 +190,7 @@ class Game:
                              (left + size // 2 - head_w / 2, (bottom - size) - size, head_w, (size - torso_h)))
 
             # hands
-            if self.jump_velocity == 0:
+            if self.player_jump_velocity == 0:
                 pygame.draw.rect(self.screen, PLAYER_COLOR.get("skin"), (left, shoulder_height, leg_w, leg_h))
                 pygame.draw.rect(self.screen, PLAYER_COLOR.get("shirt"), (left, shoulder_height, leg_w, 5))
                 pygame.draw.rect(self.screen, PLAYER_COLOR.get("skin"),
@@ -187,7 +219,7 @@ class Game:
                              (left + 2 * leg_w, shoulder_height, leg_w, torso_h))
 
             # hands
-            if self.jump_velocity == 0:
+            if self.player_jump_velocity == 0:
                 pygame.draw.rect(self.screen, PLAYER_COLOR.get("skin"),
                                  (left + 2 * leg_w, shoulder_height, leg_w, leg_h))
             else:
@@ -219,7 +251,47 @@ class Game:
         part_below_2 = all_parts.get((math.ceil(self.player_position[0]), self.player_position[1] + 1))
         if (isinstance(part_below_1, Part) and part_below_1.pt == PT.BRICK) or \
                 (isinstance(part_below_2, Part) and part_below_2.pt == PT.BRICK):
-            self.jump_velocity = - (2 * self.GRAVITY * self.jump_height) ** 0.5
+            self.player_jump_velocity = - (2 * self.GRAVITY * self.player_jump_height) ** 0.5
+
+    def update_pizzas_position(self, dt):
+        for pizza_part in self.pizza_objects:
+
+            if pizza_part.lifetime < pygame.time.get_ticks():
+                self.pizza_objects.remove(pizza_part)
+
+            elif pizza_part.direction == Direction.RIGHT:
+                pizza_part.x += dt * self.pizza_velocity
+            else:  # pizza_part.direction == Direction.LEFT
+                pizza_part.x -= dt * self.pizza_velocity
+
+        self._update_pizza_collision()
+
+    def _update_pizza_collision(self):
+        for pizza_part in self.pizza_objects:
+            part_from_right = all_parts.get((math.ceil(pizza_part.x), math.ceil(pizza_part.y)))
+            part_from_left = all_parts.get((math.floor(pizza_part.x), math.ceil(pizza_part.y)))
+            pizza_padding_right = (pizza_part.x + 1) * BLOCK_SIZE + 10
+            pizza_padding_left = pizza_part.x * BLOCK_SIZE + 10
+            bounder_line_right = math.ceil(pizza_part.x) * BLOCK_SIZE
+            bounder_line_left = math.floor(pizza_part.x) * BLOCK_SIZE
+
+            block_on_right = isinstance(part_from_right, Part) and part_from_right.pt in BLOCK_PARTS and \
+                             pizza_padding_right >= bounder_line_right
+            block_on_left = isinstance(part_from_left, Part) and part_from_left.pt in BLOCK_PARTS and \
+                            pizza_padding_left >= bounder_line_left
+
+            if block_on_right or block_on_left:
+                pizza_part.lifetime -= self.pizza_lifetime
+
+            if block_on_right and part_from_right.pt in BREAKABLE_BLOCKS:
+                self._break_breakable(math.ceil(pizza_part.x), math.ceil(pizza_part.y))
+
+            if block_on_left and part_from_left.pt in BREAKABLE_BLOCKS:
+                self._break_breakable(math.floor(pizza_part.x), math.ceil(pizza_part.y))
+
+    @staticmethod
+    def _break_breakable(x, y):
+        del all_parts[(x, y)]
 
     def update_player_position(self, dt):
         padding = 7  # in pixels
@@ -230,19 +302,19 @@ class Game:
         player_padding_below = self.player_position[1] * BLOCK_SIZE + padding
         bounder_line_below = math.ceil(self.player_position[1]) * BLOCK_SIZE
 
-        block_is_below = ((isinstance(part_below_1, Part) and part_below_1.pt == PT.BRICK) or
-                          (isinstance(part_below_2, Part) and part_below_2.pt == PT.BRICK)) and \
-                         self.jump_velocity >= 0 and \
+        block_is_below = ((isinstance(part_below_1, Part) and part_below_1.pt in BLOCK_PARTS) or
+                          (isinstance(part_below_2, Part) and part_below_2.pt in BLOCK_PARTS)) and \
+                         self.player_jump_velocity >= 0 and \
                          player_padding_below >= bounder_line_below
 
         if block_is_below:
-            self.jump_velocity = 0
+            self.player_jump_velocity = 0
             self.player_position[1] = math.ceil(self.player_position[1])
 
         # apply gravity
         else:
-            self.player_position[1] += self.jump_velocity * dt + 0.5 * self.GRAVITY * dt ** 2
-            self.jump_velocity += self.GRAVITY * dt
+            self.player_position[1] += self.player_jump_velocity * dt + 0.5 * self.GRAVITY * dt ** 2
+            self.player_jump_velocity += self.GRAVITY * dt
 
         # block besides the player.
         part_from_right0 = all_parts.get((math.ceil(self.player_position[0]), math.ceil(self.player_position[1])))
@@ -255,13 +327,13 @@ class Game:
         bounder_line_right = (math.ceil(self.player_position[0])) * BLOCK_SIZE
         bounder_line_left = (math.floor(self.player_position[0])) * BLOCK_SIZE
 
-        block_on_right0 = isinstance(part_from_right0, Part) and part_from_right0.pt == PT.BRICK and \
+        block_on_right0 = isinstance(part_from_right0, Part) and part_from_right0.pt in BLOCK_PARTS and \
                           player_padding_right >= bounder_line_right
-        block_on_left0 = isinstance(part_from_left0, Part) and part_from_left0.pt == PT.BRICK and \
+        block_on_left0 = isinstance(part_from_left0, Part) and part_from_left0.pt in BLOCK_PARTS and \
                          player_padding_left >= bounder_line_left
-        block_on_right1 = isinstance(part_from_right1, Part) and part_from_right1.pt == PT.BRICK and \
+        block_on_right1 = isinstance(part_from_right1, Part) and part_from_right1.pt in BLOCK_PARTS and \
                           player_padding_right >= bounder_line_right
-        block_on_left1 = isinstance(part_from_left1, Part) and part_from_left1.pt == PT.BRICK and \
+        block_on_left1 = isinstance(part_from_left1, Part) and part_from_left1.pt in BLOCK_PARTS and \
                          player_padding_left >= bounder_line_left
 
         # player crouch
@@ -310,17 +382,51 @@ class Game:
             above_player_padding = (self.player_position[1]) * BLOCK_SIZE - padding
             above_player_part = math.floor(self.player_position[1]) * BLOCK_SIZE
 
-        block_from_above = ((isinstance(part_above0, Part) and part_above0.pt == PT.BRICK) or
-                            (isinstance(part_above1, Part) and part_above1.pt == PT.BRICK)) and \
-                           self.jump_velocity <= 0 and \
+        block_from_above = ((isinstance(part_above0, Part) and part_above0.pt in BLOCK_PARTS) or
+                            (isinstance(part_above1, Part) and part_above1.pt in BLOCK_PARTS)) and \
+                           self.player_jump_velocity <= 0 and \
                            above_player_padding <= above_player_part
 
         if block_from_above:
             self.player_stuck[2] = True
-            self.jump_velocity = 0
+            self.player_jump_velocity = 0
             self.player_position[1] = math.floor(self.player_position[1])
         else:
             self.player_stuck[2] = False
+
+    def reset(self):
+        self.player_position = [W_BLOCKS // 2, H_BLOCKS - 3]
+        self.player_velocity = PLAYER_VELOCITY.get("normal")
+        self.player_crouch = False
+        self.player_stuck = [False, False, False]  # [from_left, from_right, from_top]
+        self.player_direction = Direction.FRONT
+        self.player_jump_height = PLAYER_JUMP_H.get("normal")
+        self.player_jump_velocity = 0
+        self.init_floor()
+        Maps.map_0()
+
+    def apply_game_rules(self):
+
+        if self.player_position[1] > H_BLOCKS + 1:
+            self.reset()
+
+    def _throw_pizza(self):
+        if not self.player_direction == Direction.FRONT:
+
+            padding = 1  # throws the pizza one block away from the player
+            if self.player_direction == Direction.LEFT:
+                padding *= -1
+
+            if not self.player_crouch:
+                self.pizza_objects.append(Part(self.player_position[0] + padding,
+                                               math.floor(self.player_position[1] - 1),
+                                               PT.PIZZA, pygame.time.get_ticks() + self.pizza_lifetime,
+                                               self.player_direction))
+            else:
+                self.pizza_objects.append(Part(self.player_position[0] + padding,
+                                               math.floor(self.player_position[1]),
+                                               PT.MINI_PIZZA, pygame.time.get_ticks() + self.pizza_lifetime,
+                                               self.player_direction))
 
 
 class Maps:
@@ -335,28 +441,28 @@ class Maps:
             for j in range(5):
                 Part(x + 3 + 5 * i + j, y - i, PT.BRICK)
 
-        Part(x + 50, y, PT.BRICK)
-        Part(x + 50, y - 1, PT.BRICK)
+        for i in range(4):
+            for j in range(5):
+                Part(x - 3 - 5 * i - j, y - i, PT.BOX)
+
+        Part(x + 50, y, PT.BOX)
+        Part(x + 50, y - 1, PT.BOX)
 
 
 def main():
     game = Game(WIDTH, HEIGHT)
 
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
+    while game.running:
         dt = game.clock.tick(60) / 1000
 
-        game.screen.fill(PART_COLOR.get(PT.BACKGROUND))
+        game.screen.fill(PART_COLOR.get("background"))
 
         game.init_floor()
         game.draw_parts()
-
+        game.apply_game_rules()
         game.handle_keys(dt)
         game.update_player_position(dt)
+        game.update_pizzas_position(dt)
         game.draw_player()
         pygame.display.flip()
 
